25554293fefead11df36287948e0abdcdef0ec58
Erkki Seppälä <flux@modeemi.fi>
2015-08-23 15:18:34 +0300

    myocamlbuild: use ocamlfind

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -2,6 +2,8 @@ open Ocamlbuild_plugin
 open Command
 
 let _ = dispatch begin function
+  | Before_options ->
+     Options.use_ocamlfind := true
   | After_rules ->
       flag ["ocaml"; "compile"; "no_warn_40"] (S[A"-w"; A"-40"]);
 

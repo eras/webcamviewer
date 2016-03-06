0ed3d7a378bba7032c159d802d7da7dfd608b00e
Erkki Seppälä <flux@modeemi.fi>
2016-03-06 18:03:42 +0200

    HttpChunkStream: added a small bit of documentation

--- httpChunkStream.mli
+++ httpChunkStream.mli
@@ -1,3 +1,7 @@
+(** [start ~on_eof http_mt url callback] starts retrieving the given
+    URL, calling callback on received chunks. When an end-of-file is
+    reached the on_eof handler is called (ie. in a image streaming
+    case the handler might restart the transfer). *)
 val start :
   on_eof:(unit -> unit) ->
   < multi : Curl.Multi.mt;

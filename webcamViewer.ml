818274326cfee67a4980b7ef78172398ecbaf94e
Erkki Seppälä <flux@modeemi.fi>
2016-03-06 13:48:34 +0200

    WebcamViewer.read_config_file: now works with Toml 4.0.0

--- webcamViewer.ml
+++ webcamViewer.ml
@@ -9,26 +9,41 @@ let destroy () =
 (*   let config_file = create_options_file (Unix.getenv "HOME" ^ "/.webcamviewer2") in *)
 (*   let streams = new group  *)
 
+module List_of_Map (Map: Legacy.Map.S) =
+struct
+  let map m = Map.fold (fun (key : Map.key) value xs -> (key, value)::xs) m [] |> List.rev
+end
+
+let map_fst f xs = List.map (fun (k, v) -> (f k, v)) xs
+
 let read_config_file () =
   let open Toml in
   let config_file = Unix.getenv "HOME" ^ "/.webcamviewer" in
-  let general, cameras = List.partition (fun (name, _) -> name = "general") @@ tables_to_list (from_filename config_file) in
-  let sources =
-    cameras |> List.map @@ fun (name, camera_config) ->
-      { source_url = get_string camera_config "url";
-        source_name = name; } in
-  let default_general_option key default =
-    match general with
-    | [] -> default
-    | (_, general)::_ ->
-      try get_string general key
-      with Not_found -> default
-  in
-  let config = {
-    config_sources = sources;
-    config_output_base = default_general_option "output" "output";
-  } in
-  config
+  match Parser.from_filename config_file with
+  | `Error (message, location) ->
+     Printf.eprintf "Error while reading configuration: %s at line %d column %d\n"
+       location.source
+       location.line
+       location.column;
+    None
+  | `Ok config ->
+     let config = let module M = List_of_Map (TomlTypes.Table) in M.map config |> map_fst TomlTypes.Table.Key.to_string in
+     let general, cameras = List.partition (fun (name, _) -> name = "general") @@ config in
+     let sources =
+       cameras |> List.map @@ fun (name, camera_config) ->
+         { source_url = Option.get (TomlLenses.(get camera_config (table |-- key "url" |-- string)));
+           source_name = name; } in
+     let default_general_option key_ default =
+       match general with
+       | [] -> default
+       | (_, general)::_ ->
+          Option.default default @@ TomlLenses.(get general (table |-- key key_ |-- string))
+     in
+     let config = {
+       config_sources = sources;
+       config_output_base = default_general_option "output" "output";
+     } in
+     Some config
 
 (* let read_streams () = File.lines_of (Unix.getenv "HOME" ^ "/.webcamviewer") |> List.of_enum *)
 
@@ -40,9 +55,12 @@ let main () =
   let vbox = GPack.vbox ~packing:main_window#add () in
   let quit_button = GButton.button ~label:"Quit" ~packing:(vbox#pack ~expand:false) () in
   ignore (quit_button#connect#clicked ~callback:destroy);
-  let config = read_config_file () in
-  List.iter (fun source -> ignore (StreamView.view config source http_mt ~packing:vbox#add ())) config.config_sources;
-  main_window#show ();
-  GMain.Main.main ()
+  match read_config_file () with
+  | Some config ->
+     List.iter (fun source -> ignore (StreamView.view config source http_mt ~packing:vbox#add ())) config.config_sources;
+     main_window#show ();
+     GMain.Main.main ()
+  | None ->
+     ()
 
 let _ = main ()
